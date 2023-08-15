35a38f3b74bfbc2336e9d1b33e2e3e57e5247313
zl358 <neil.lu@duke.edu>
2023-08-15 17:13:34 +0800

    added 4-loops

---
+++ problems/5-multiplication-table.py
@@ -0,0 +1,14 @@
+"""
+Practice Exercise 5: Multiplication Table Generator
+
+Create a program that generates multiplication tables using loops and functions:
+
+`generate_table(number, num_terms)`: 
+    Generates and returns a multiplication table for the given number with the specified number of terms.
+
+Your program should:
+
+1. Ask the user for a number and the number of terms they want in the multiplication table.
+2. Call the generate_table function to generate the table.
+3. Display the generated multiplication table.
+"""
