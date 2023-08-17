2185ea0049b2e002eee84e5c5e93badc95b00f9e
zl358 <neil.lu@duke.edu>
2023-08-17 17:15:43 +0800

    added exercise 8

---
+++ problems/8-reverse-list.py
@@ -0,0 +1,9 @@
+"""
+Practice exercise 8: Reverse List 
+
+Your program should: 
+
+1. Prompt the user to input a list of 8 numbers.
+2. Reverse the list using `for` or any form of loop, but not [::-1] or reverse().
+3. Print the reversed list. 
+"""
